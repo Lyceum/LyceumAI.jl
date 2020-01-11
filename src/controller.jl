728772895771da3019ec58d6a40561a510725c3c
Colin Summers <colinxs@cs.washington.edu>
2020-01-10 18:11:53 -0800

    add ability to random start in ControllerIterator (#13)

--- src/controller.jl
+++ src/controller.jl
@@ -5,12 +5,14 @@ struct ControllerIterator{C,E,B}
     T::Int
     trajectory::B
     plotiter::Int
+    randstart::Bool
 
     function ControllerIterator(
         controller,
         env::AbstractEnvironment;
-        T = 1000,
-        plotiter = 1,
+        T::Integer = 1000,
+        plotiter::Integer = 1,
+        randstart::Bool = false
     )
         trajectory = (
             states = Array(undef, statespace(env), T),
@@ -25,33 +27,41 @@ struct ControllerIterator{C,E,B}
             T,
             trajectory,
             plotiter,
+            randstart
         )
     end
 end
 
-function Base.iterate(m::ControllerIterator, t)
-    t > m.T && return
+function Base.iterate(it::ControllerIterator, t)
+    t > it.T && return
 
-    rolloutstep!(m.controller, m.trajectory, m.env, t)
-    if mod(t, m.plotiter) == 0
-        rew = @views Tools.Line(m.trajectory.rewards[1:t], "Reward")
-        eval = @views Tools.Line(m.trajectory.evaluations[1:t], "Eval")
+    rolloutstep!(it.controller, it.trajectory, it.env, t)
+    if mod(t, it.plotiter) == 0
+        rew = @views Tools.Line(it.trajectory.rewards[1:t], "Reward")
+        eval = @views Tools.Line(it.trajectory.evaluations[1:t], "Eval")
         plt = Tools.expplot(
             rew,
             eval,
-            title = "ControllerIterator Iteration=$t/$(m.T)",
+            title = "ControllerIterator Iteration=$t/$(it.T)",
             width = 40,
         )
         display(plt)
     end
 
-    (t, m), t + 1
+    (t, it), t + 1
 end
 
-Base.iterate(m::ControllerIterator) = (reset!(m.env); reset!(m.controller); iterate(m, 1))
-Base.length(m::ControllerIterator) = m.T
+function Base.iterate(it::ControllerIterator)
+    it.randstart ? randreset!(it.env) : reset!(it.env)
+    error(getstate(it.env))
+    reset!(it.controller)
+    iterate(it, 1)
+end
+
+Base.length(it::ControllerIterator) = it.T
 
 function rolloutstep!(controller, traj, env, t)
+    # TODO uviews
     st = view(traj.states, .., t)
     ot = view(traj.observations, .., t)
     at = view(traj.actions, .., t)
